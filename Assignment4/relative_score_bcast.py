9b7e9965fd934a3923f069728054ce5f4cddb344
Jaskaran23 <47342928+Jaskaran23@users.noreply.github.com>
2019-02-05 09:49:06 -0800

    Create relative_score_bcast.py

---
+++ Assignment4/relative_score_bcast.py
@@ -0,0 +1,59 @@
+from pyspark import SparkConf, SparkContext
+import sys
+import json
+
+assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
+
+
+def key_valuepair(dictvalue):
+	reddit_key=dictvalue.get("subreddit")
+	score_key=dictvalue.get("score")
+	yield (reddit_key,(1,score_key))
+
+def adding_pairs(i,j):
+	sumone=0
+	scoresum=0
+	sumone = i[0]+j[0]
+	scoresum = i[1]+j[1]
+	return (sumone,scoresum) 
+
+def dividing_pairs(k):
+	average = 0.0
+	average = (k[1][1]/k[1][0])
+	return(k[0],average)
+
+def broadcast_func(broadcast_obj1,y):
+	subreddit_val=y[0]
+	return(y[1]['score'] / broadcast_obj1.value[y[0]],y[1]['author'])
+		
+
+
+
+
+def main(inputs, output):
+	text=sc.textFile(inputs).map(json.loads).cache()
+	pairs=text.flatMap(key_valuepair)
+	add_pair=pairs.reduceByKey(adding_pairs)
+	avg_pair=add_pair.map(dividing_pairs).filter(lambda x: (x[1]>0))
+
+	avg_obj=dict(avg_pair.collect())
+	broadcast_obj=sc.broadcast(avg_obj)
+
+	commentbysub = text.map(lambda c: (c['subreddit'],c))
+	result=commentbysub.map(lambda x: broadcast_func(broadcast_obj,x)).sortBy(lambda z: z,False)
+	print(result.take(10))
+	#result=joined_rdd.map(lambda z: (z[0],z[1][0][0]/z[1][1],z[1][0][1])).sortBy(False)
+	
+
+	outdata=result.map(json.dumps)
+	outdata.saveAsTextFile(output)
+
+
+
+if __name__ == '__main__':
+    conf = SparkConf().setAppName('reddit average')
+    sc = SparkContext(conf=conf)
+    assert sc.version >= '2.3'  # make sure we have Spark 2.3+
+    inputs = sys.argv[1]
+    output = sys.argv[2]
+    main(inputs, output)
